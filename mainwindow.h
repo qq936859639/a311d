165fa4496cf743a5f87a2530c7754203f007f8ac
qq936859639 <936859639@qq.com>
2022-03-21 16:03:38 +0800

    20220321 v1.0

---
+++ mainwindow.h
@@ -0,0 +1,44 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_btcamera_clicked();
+
+    void on_btcar_clicked();
+
+    void on_btlcd_clicked();
+
+    void on_btmic_clicked();
+
+    void on_bta311d_clicked();
+
+    void on_btkeyboard_clicked();
+
+    void on_btgps_clicked();
+
+    void on_btsonor_clicked();
+
+    void on_btspeaker_clicked();
+
+    void on_btiotdht_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
